33c56811228bb4792fa22490d248813f70aa0dac
qjex <theqjex@gmail.com>
2019-01-12 14:12:28 +0300

    ce on ambig get

--- tuple.h
+++ tuple.h
@@ -5,6 +5,8 @@
 #ifndef TUPLE_TUPLE_H
 #define TUPLE_TUPLE_H
 
+#include <cstddef>
+
 template<typename Head, typename... Tail>
 struct tuple : public tuple<Tail...> {
     tuple(Head head, Tail... tail) : tuple<Tail...>(tail...), head(head) {}
@@ -59,22 +61,64 @@ struct getTypeImpl<Head, Head, Tail...> {
     }
 };
 
+template<size_t cnt, typename Target, typename Head, typename... Tail>
+struct checkImpl {
+    static void check(const tuple<Head, Tail...> &t) {
+        checkImpl<cnt, Target, Tail...>::check(t);
+    }
+};
+
+template<typename Target, typename Head>
+struct checkImpl<0, Target, Head> {
+    static void check(const tuple<Head> &t) {
+    }
+};
+
+template<typename Target, typename Head>
+struct checkImpl<1, Target, Head> {
+    static void check(const tuple<Head> &t) {
+    }
+};
+
+template<typename Target, typename Head>
+struct checkImpl<2, Target, Head> {
+};
+
+template<typename Head>
+struct checkImpl<0, Head, Head> {
+    static void check(const tuple<Head> &t) {
+    }
+};
+
+template<typename Head>
+struct checkImpl<1, Head, Head> {
+};
+
+template<size_t cnt, typename Head, typename... Tail>
+struct checkImpl<cnt, Head, Head, Tail...> {
+    static void check(const tuple<Head, Tail...> &t) {
+        checkImpl<cnt + 1, Head, Tail...>::check(t);
+    }
+};
+
 template<typename Target, typename Head, typename... Tail>
 auto &get(tuple<Head, Tail...> &t) {
+    checkImpl<0, Target, Head, Tail...>::check(t);
     return getTypeImpl<Target, Head, Tail...>::value(t);
 }
 
-template<int index, typename Head, typename... Tail>
+template<size_t index, typename Head, typename... Tail>
 auto &get(tuple<Head, Tail...> &t) {
     return getImpl<index, Head, Tail...>::value(t);
 }
 
 template<typename Target, typename Head, typename... Tail>
 auto &get(const tuple<Head, Tail...> &t) {
+    checkImpl<0, Target, Head, Tail...>::check(t);
     return getTypeImpl<Target, Head, Tail...>::value(t);
 }
 
-template<int index, typename Head, typename... Tail>
+template<size_t index, typename Head, typename... Tail>
 auto &get(const tuple<Head, Tail...> &t) {
     return getImpl<index, Head, Tail...>::value(t);
 }
